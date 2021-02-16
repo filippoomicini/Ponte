ba55bad1d65c3d9d410c5f7809a47427048e3d3e
filippoomicini <filippo.omicini.stud@ispascalcomandini.it>
2021-02-16 11:11:06 +0100

    implementazione movimento macchina arancione

--- ponte/ponte/MainWindow.xaml.cs
+++ ponte/ponte/MainWindow.xaml.cs
@@ -23,28 +23,52 @@ namespace ponte
     {
         private static object x = new object();
         Random rnd = new Random();
+        int mV = -140;
+        int mASinistro = 586;
+        int mADestro = -132;
+
         public MainWindow()
         {
-            Thread t1 = new Thread(new ThreadStart(MovimentoMacchina1));
-            Thread t2 = new Thread(new ThreadStart(MovimentoMacchina2));
+            Thread t1 = new Thread(new ThreadStart(MovimentoMacchinaA));
+            Thread t2 = new Thread(new ThreadStart(MovimentoMacchinaV));
             t1.Start();
             t2.Start();
 
         }
 
-        public void MovimentoMacchina1()
+        public void MovimentoMacchinaA()
         {
             lock (x)
             {
                 int a = rnd.Next(0, 20);
                 if (a > 10)
                 {
-                    //movimento macchina1
+                    mASinistro = mASinistro - 10;
+                    mADestro = mADestro + 10;
+                    macchinaA.Margin = new Thickness(mASinistro, 30, mADestro, 138);
+                    if(macchinaA.Margin == new Thickness(430, 30, 24, 138))
+                    {
+                        semaforoVerdeA.Visibility = Visibility.Visible;
+                        semaforoRossoV.Visibility = Visibility.Visible;
+                        int mASinistro2 = 430;
+                        int mADestro2 = 138;
+                        while(macchinaA.Margin != new Thickness(10, 30, 444, 138))
+                        {
+                            mASinistro2 = mASinistro2 - 10;
+                            mADestro2 = mADestro2 + 10;
+                            macchinaA.Margin = new Thickness(mASinistro2, 30, mADestro2, 138);
+
+                        }
+                        semaforoVerdeA.Visibility = Visibility.Hidden;
+                        semaforoVerdeV.Visibility = Visibility.Visible;
+                        semaforoRossoA.Visibility = Visibility.Visible;
+                        semaforoRossoV.Visibility = Visibility.Hidden;
+                    }
                 }
             }
         }
 
-        public void MovimentoMacchina2()
+        public void MovimentoMacchinaV()
         {
             lock (x)
             {
