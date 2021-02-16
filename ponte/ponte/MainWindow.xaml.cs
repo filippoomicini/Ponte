454112afa030a11cf4b6e685d373fef8e84fd83d
filippoomicini <filippo.omicini.stud@ispascalcomandini.it>
2021-02-16 12:04:07 +0100

    implementazione movimento macchina verde

--- ponte/ponte/MainWindow.xaml.cs
+++ ponte/ponte/MainWindow.xaml.cs
@@ -23,16 +23,20 @@ namespace ponte
     {
         private static object x = new object();
         Random rnd = new Random();
-        int mV = -140;
         int mASinistro = 586;
         int mADestro = -132;
-
+        int mVSinistro = -140;
+        int mVDestro = 507;
         public MainWindow()
         {
+            InitializeComponent();
             Thread t1 = new Thread(new ThreadStart(MovimentoMacchinaA));
             Thread t2 = new Thread(new ThreadStart(MovimentoMacchinaV));
-            t1.Start();
-            t2.Start();
+            while(macchinaA.Margin != new Thickness(104,30,558,138) && macchinaV.Margin != new Thickness(530,66,164,123))
+            {
+                t1.Start();
+                t2.Start();
+            }
 
         }
 
@@ -46,13 +50,13 @@ namespace ponte
                     mASinistro = mASinistro - 10;
                     mADestro = mADestro + 10;
                     macchinaA.Margin = new Thickness(mASinistro, 30, mADestro, 138);
-                    if(macchinaA.Margin == new Thickness(430, 30, 24, 138))
+                    if (macchinaA.Margin == new Thickness(430, 30, 24, 138))
                     {
                         semaforoVerdeA.Visibility = Visibility.Visible;
                         semaforoRossoV.Visibility = Visibility.Visible;
                         int mASinistro2 = 430;
-                        int mADestro2 = 138;
-                        while(macchinaA.Margin != new Thickness(10, 30, 444, 138))
+                        int mADestro2 = 24;
+                        while (macchinaA.Margin != new Thickness(10, 30, 444, 138))
                         {
                             mASinistro2 = mASinistro2 - 10;
                             mADestro2 = mADestro2 + 10;
@@ -75,9 +79,32 @@ namespace ponte
                 int a = rnd.Next(0, 20);
                 if (a > 10)
                 {
-                    //movimento macchina2
+                    mVSinistro = mVSinistro + 10;
+                    mVDestro = mVDestro - 10;
+                    macchinaV.Margin = new Thickness(mVSinistro, 66, mVDestro, 123);
+                    if (macchinaV.Margin == new Thickness(-28, 66, 394, 123))
+                    {
+                        semaforoVerdeV.Visibility = Visibility.Visible;
+                        semaforoRossoA.Visibility = Visibility.Visible;
+                        int mVSinistro2 = -28;
+                        int mVDestro2 = 394;
+                        while (macchinaV.Margin != new Thickness(378, 66, 12, 123))
+                        {
+                            mVSinistro2 = mVSinistro2 + 10;
+                            mVDestro2 = mVDestro2 - 10;
+                            macchinaV.Margin = new Thickness(mVSinistro2, 66, mVDestro2, 123);
+
+                        }
+                        semaforoVerdeV.Visibility = Visibility.Hidden;
+                        semaforoVerdeA.Visibility = Visibility.Visible;
+                        semaforoRossoV.Visibility = Visibility.Visible;
+                        semaforoRossoA.Visibility = Visibility.Hidden;
+                    }
                 }
             }
         }
     }
 }
+
+
+
